c6a43483d7157beb7872d6f8ede3f7b67b538591
Mattar17 <mattar17x@gmail.com>
2025-12-16 17:43:14 +0200

    Load more posts then scroll to the loaded post

--- src/Posts/Posts.js
+++ src/Posts/Posts.js
@@ -3,15 +3,20 @@ import { MoonLoader } from "react-spinners";
 import Post from "./Post";
 
 export default function Posts({ posts, setPosts }) {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [LoadingMore, setLoadingMore] = useState(false);
   const [Page, setPage] = useState(1);
   const fetchedPage = useRef(new Set());
+  const scrollIntoIndex = useRef(0);
 
   const api_url = process.env.REACT_APP_API_URL;
 
   useEffect(() => {
+    let ignore = false;
     async function fetchData() {
+      if (ignore) return;
       setIsLoading(true);
+      if (Page > 1) setLoadingMore(true);
       if (fetchedPage.current.has(Page)) return;
       fetchedPage.current.add(Page);
 
@@ -19,14 +24,23 @@ export default function Posts({ posts, setPosts }) {
       const { posts } = await response.json();
       if (!posts) return;
       setPosts((current) => {
-        const combined = [...current, ...posts];
-        console.log(typeof combined);
-        return combined;
+        scrollIntoIndex.current = current.length;
+        return [...current, ...posts];
       });
+      setTimeout(() => {
+        const el = document.getElementById(`post-${scrollIntoIndex.current}`);
+        el?.scrollIntoView({ behavior: "smooth", block: "start" });
+        console.log("tried to scroll");
+      }, 0);
       setIsLoading(false);
+      setLoadingMore(false);
     }
 
     fetchData();
+
+    return () => {
+      ignore = true;
+    };
   }, [Page, api_url]);
 
   const handleSetPage = function () {
@@ -35,25 +49,19 @@ export default function Posts({ posts, setPosts }) {
 
   return (
     <div className="relative mt-[40px]">
-      {isLoading ? (
-        <MoonLoader className="mx-auto" color="#981316" />
-      ) : (
-        posts?.map((post, i) => (
-          <Post id={`post-${i}`} key={post._id} postDetails={post} />
-        ))
-      )}
+      {isLoading && <MoonLoader className="mx-auto" color="#981316" />}
+      {posts?.map((post, i) => (
+        <Post id={`post-${i}`} key={post._id} postDetails={post} />
+      ))}
 
-      {isLoading && Page > 1 ? (
-        <MoonLoader className="mx-auto" color="#981316" />
-      ) : (
-        <button
-          onClick={handleSetPage}
-          className="relative mx-auto flex justify-center items-center"
-        >
-          <span>Load More Posts</span>
-          <span className="text-xl mt-[13px] ml-[2px]">&#129171;</span>
-        </button>
-      )}
+      {LoadingMore && <MoonLoader className="mx-auto" color="#981316" />}
+      <button
+        onClick={handleSetPage}
+        className="relative mx-auto flex justify-center items-center"
+      >
+        <span>Load More Posts</span>
+        <span className="text-xl mt-[13px] ml-[2px]">&#129171;</span>
+      </button>
     </div>
   );
 }
