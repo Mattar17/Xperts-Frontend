c2719faf3b478565d674bfc49b0bff3cca52dace
Mattar17 <mattar17x@gmail.com>
2026-01-12 22:00:19 +0200

    responsive logo + Navigate to home when logo is clicked

--- src/Navbar/SearchBar.js
+++ src/Navbar/SearchBar.js
@@ -1,13 +1,16 @@
+import { NavLink } from "react-router-dom";
 import SearchInput from "./SearchInput";
 
 export default function SearchBar() {
   return (
     <div className="flex justify-between left-[20px] items-center">
-      <img
-        className="w-[135px] h-[150%]"
-        src="https://res.cloudinary.com/dus5jhwhc/image/upload/v1763025555/Xperts_Logo__1_ey70sd.png"
-        alt="Logo"
-      ></img>
+      <NavLink to="/">
+        <img
+          className="sm:w-[135px] sm:h-[150%] w-[90px] h-[100%]"
+          src="https://res.cloudinary.com/dus5jhwhc/image/upload/v1763025555/Xperts_Logo__1_ey70sd.png"
+          alt="Logo"
+        ></img>
+      </NavLink>
       <SearchInput className="search_input" />
     </div>
   );
